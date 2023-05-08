71fbd8dca2f872c115989ca1192c9d9be15340aa
Leo <leoedwardsdeveloper@gmail.com>
2023-05-08 21:26:33 +0100

    Adds a command for tinkering

---
+++ app/Console/Commands/ImportPlayers.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\SportmonksFootball\Client;
+use Illuminate\Console\Command;
+
+class ImportPlayers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:import-players';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(Client $client)
+    {
+        //
+        (new \App\Services\SportmonksFootball\Actions\ImportPlayers())->__invoke($client);
+    }
+}

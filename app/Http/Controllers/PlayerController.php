ea6e61c3d6b0f1cbbd240ed32a9965afc99b453b
Leo <leoedwardsdeveloper@gmail.com>
2023-05-09 21:54:17 +0100

    Adds a UI index

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+
+use App\Models\Player;
 
 class PlayerController extends Controller
 {
     //
+    public function index(): \Illuminate\Contracts\View\Factory|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
+    {
+        $players = Player::query()->with('country')->paginate(10);
+
+        return  view('players.index', ['players' => $players]);
+    }
 }
---
+++ resources/views/players/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+@section('content')
+    <h2 class="mb-4">Players</h2>
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th scope="col">Display Name</th>
+            <th scope="col">Gender</th>
+            <th scope="col">Age</th>
+            <th scope="col">Position</th>
+            <th scope="col">Country</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach ($players as $player)
+            <tr>
+                <td>{{ $player->display_name }}</td>
+                <td>{{ $player->gender}}</td>
+                <td>{{ $player->date_of_bith }}</td>
+                <td>{{ $player->position }}</td>
+                <td>{{ $player->country->name }}</td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+@endsection
+

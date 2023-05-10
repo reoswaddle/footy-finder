291c63224771ac82462ff395bd6baaf4da4735c7
Leo <leoedwardsdeveloper@gmail.com>
2023-05-10 18:33:36 +0100

    shows name field

--- resources/views/players/show.blade.php
+++ resources/views/players/show.blade.php
@@ -9,8 +9,9 @@
                 <div class="card-body">
                     <h5 class="card-title"> <img width="25" class="me-1" src="{{ $player->country->imagePath }}"  alt="{{ $player->country->name }} Flag"/> {{$player->displayName}}</h5>
                     <ul class="list-group list-group-flush">
+                        <li class="list-group-item"> Name: {{ "$player->firstName $player->lastName" }}</li>
                         <li class="list-group-item"> Country: {{ $player->country->name }} </li>
-                        <li class="list-group-item"> Country: {{ $player->position }} </li>
+                        <li class="list-group-item"> Position: {{ $player->position }} </li>
                         <li class="list-group-item"> Age: {{ $player->age }} </li>
                         <li class="list-group-item"> Birthday: {{ $player->birthday }} </li>
                         <li class="list-group-item"> <a href="{{ url()->previous() }}" class="btn btn-secondary"> Back </a></li>
